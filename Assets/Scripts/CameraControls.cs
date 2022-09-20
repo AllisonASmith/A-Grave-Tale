bcfd2251832fa669888d90aa8e1713a817853f14
AllisonASmith <allisos1@umbc.edu>
2022-09-20 01:17:03 -0400

    Add Project

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public GameObject target; // the focus for the camera
+    public bool isSmoothed; // true if the camera isn't directly following a player or enemy -- works best on a static target
+ 
+    void Update()
+    {
+        // if smooth, camera will lerp to target position
+        // otherwise camera snaps to target
+        transform.position = isSmoothed ? Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10), .025f) : new Vector3(target.transform.position.x, target.transform.position.y, -10);
+    }
+}
---
+++ Assets/Scripts/Movement2DSide.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement2DSide : MonoBehaviour
+{
+    // player components
+    Rigidbody2D rb; // physics and collision
+    Animator anim; // animation
+    SpriteRenderer sr; // base sprite
+
+    public float Scale; //scale of the game (affects speed, later attack speed)
+    [Range (0,1)]
+    public float speed; //speed magnitude
+    [Range (0,5)]
+    public float atkSpeed; // attack cooldown
+    [Range (0, 5)]
+    public float Friction; //stops object on the floor, use 0 for no friction
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        speed *= Scale;
+        speed *= atkSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // keys/joysticks for inputs can be managed in Edit > Project Settings > Input Manager
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        anim.SetInteger("x", Mathf.CeilToInt(x));
+        anim.SetInteger("y", Mathf.CeilToInt(y));
+
+        // movement
+        rb.velocity = new Vector2(x * speed, y * speed);
+    }
+}
