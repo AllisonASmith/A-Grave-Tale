2aee93fb249a1e7253ed91d46a4c641b7131766d
irule4567 <33972693+irule4567@users.noreply.github.com>
2022-10-07 14:12:51 -0400

    Invincibility Frames
    Added Invincibility frame functionality after being hit by an enemy. This is only in TestSceneCollision.

--- Assets/Scripts/DaveStats.cs
+++ Assets/Scripts/DaveStats.cs
@@ -6,26 +6,32 @@ public class DaveStats : MonoBehaviour
 {
     [Range (0, 10)]
     public int daveHealth;
+    int onHitInvincibleFrames;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        onHitInvincibleFrames = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(daveHealth == 0){
-            Debug.Log("dead");
+        //Debug.Log("Invincibility Frames: " + onHitInvincibleFrames);
+        if(onHitInvincibleFrames > 0){
+            //Debug.Log("Invincible");
+            onHitInvincibleFrames--;
         }
+        //if(daveHealth == 0){
+            //Debug.Log("dead");
+        //}
     }
-    void OnTriggerEnter2D(Collider2D col){
-        if(col.name == "Enemy"){
-            Debug.Log("collision detected");
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.name == "Enemy" & onHitInvincibleFrames == 0){
+            //Debug.Log("collision detected");
             daveHealth--;
+            onHitInvincibleFrames = 240;
         }
-    }
-    public int getHealth(){
-        return daveHealth;
     }
 }
